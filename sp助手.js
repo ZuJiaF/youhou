0466eb23427248519e797a4608daf3babea4f073
ZuJiaF <148544942+ZuJiaF@users.noreply.github.com>
2024-05-12 20:49:34 +0800

    Update sp助手.js

--- sp助手.js
+++ sp助手.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         sp助手
 // @namespace    http://tampermonkey.net/
-// @version      0.4.23
+// @version      0.4.24
 // @description  try to take over the world!
 // @author       You
 // @match        https://shopee.co.th/*
